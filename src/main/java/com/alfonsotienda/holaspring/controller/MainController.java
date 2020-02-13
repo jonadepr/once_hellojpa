ced9962b38e250d1bd852c3e39f063d424f091e2
jonadepr <eljosele4@gmail.com>
2020-02-13 20:07:14 +0100

    ultimo 2.0

--- src/main/java/com/alfonsotienda/holaspring/controller/MainController.java
+++ src/main/java/com/alfonsotienda/holaspring/controller/MainController.java
@@ -1,9 +1,12 @@
 package com.alfonsotienda.holaspring.controller;
 
-import com.alfonsotienda.holaspring.model.Clientes;
-import com.alfonsotienda.holaspring.model.ClientesRepository;
+import javax.persistence.EntityManager;
+
+import com.alfonsotienda.holaspring.model.Cliente;
+import com.alfonsotienda.holaspring.model.ClienteRepository;
 import com.alfonsotienda.holaspring.model.Factura;
 import com.alfonsotienda.holaspring.model.FacturaRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,11 +19,17 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
- * MainController en controller
+ * MainController
  */
 
 @Controller
 public class MainController {
+
+    @Autowired
+    FacturaRepository facturaRepository;
+
+    @Autowired
+    ClienteRepository clienteRepository;
 
     @GetMapping("/")
     @ResponseBody
@@ -28,48 +37,65 @@ public class MainController {
         return "Hola " + name + " tienes " + edad + "años";
     }
 
-    @Autowired
-    FacturaRepository facturaRepository; //FacturaRepository maneja los datos de la bd
+
+
+
+
+
 
     @GetMapping("/creafactura")
     @ResponseBody
-    public ResponseEntity<String> creafactura(
-        @RequestParam("fecha") String fecha, 
-        @RequestParam("id") Integer id, 
-        @RequestParam("total") Double total) 
-        {
-            Factura factura = new Factura();
-            factura.setFecha(fecha);
-            factura.setId(id);
-            factura.setTotal(total);
-            facturaRepository.save(factura);
-            ResponseEntity<String> responseEntity =
-            new ResponseEntity<String>(HttpStatus.CREATED);
-            return responseEntity;
-        }
-
-
-        @Autowired
-        ClientesRepository clientesRepository; //ClienteRepository maneja los datos de la bd
-    
-        @GetMapping("/creaclientes") 
-        @ResponseBody
-        public ResponseEntity<String> creaclientes(
-            @RequestParam("nombre") String nombre, 
-            @RequestParam(value="id", required = false) Integer id, 
-            @RequestParam("apellidos") String apellidos,
-            @RequestParam("edad") Integer edad) 
-            {
-                Clientes cliente = new Clientes();
-                cliente.setNombre(nombre);
-                cliente.setId(id);
-                cliente.setApellidos(apellidos);
-                cliente.setEdad(edad);
-                clientesRepository.save(cliente);
-                ResponseEntity<String> responseEntity =
-                new ResponseEntity<String>(HttpStatus.CREATED);
+    public ResponseEntity creaFactura(
+            @RequestParam("fecha") String fecha,
+            @RequestParam("id") Integer id,
+            @RequestParam("total") Double total
+            ) {
+                Factura factura = new Factura();
+                factura.setFecha(fecha);
+                factura.setId(id);
+                factura.setTotal(total);
+                facturaRepository.save(factura);
+                
+                ResponseEntity responseEntity = 
+                    new ResponseEntity<>(HttpStatus.CREATED);
                 return responseEntity;
-            }
+    }
+
+    @GetMapping("/cliente")
+    @ResponseBody
+    public ModelAndView creaCliente() {
+
+        ModelAndView modelAndView=new ModelAndView("cliente");
+        modelAndView.addObject("mensaje", "");
+        return modelAndView;
+    }
+
+    @PostMapping("/cliente")
+    public ModelAndView clientePost(
+        @RequestParam("nombre") String nombre,
+        @RequestParam("apellido") String apellido,
+        @RequestParam("edad") Integer edad
+    ){
+        ModelAndView modelAndView=new ModelAndView("cliente");
+        Cliente cliente=new Cliente(nombre, apellido, edad);
+        clienteRepository.save(cliente);
+
+        return modelAndView;
+    }
+
+    @GetMapping("/lista")
+    public ModelAndView showListaClientes(){
+        ModelAndView modelAndView = new ModelAndView("listaCli");
+        Iterable<Cliente> todosLosClientes = clienteRepository.findAll();
+
+        String todos = "";
+        for (Cliente cliente : todosLosClientes) {
+            todos = todos + cliente.getNombre()+ ", ";
+        }
+        modelAndView.addObject("clientes", todosLosClientes);
+        modelAndView.addObject("todos", todos);
+        return modelAndView;
+    }   
 
     @GetMapping("/ingles")
     @ResponseBody
@@ -186,7 +212,8 @@ public class MainController {
 
     @PostMapping("/test")
     @ResponseBody
-    public ModelAndView testlocuraHTMLPost(@RequestParam("respuesta1") String respuesta1,
+
+    public ModelAndView testloculraHTMLPost(@RequestParam("respuesta1") String respuesta1,
             @RequestParam("respuesta2") String respuesta2, @RequestParam("respuesta3") String respuesta3,
             @RequestParam("respuesta4") String respuesta4, @RequestParam("respuesta5") String respuesta5) {
 
@@ -275,19 +302,4 @@ public class MainController {
         }
 
     }
-
-/**
- * clientesRepository.findAll() mirar los demás después del punto
- * Devuelve Iterable<T> y findAll() Returns all instances of the type.
- */
-    @GetMapping("/listado")
-    public ModelAndView listadoClientes() {
-        ModelAndView modelAndView = new ModelAndView("listado"); 
-        Iterable<Clientes> cli = clientesRepository.findAll();
-        modelAndView.addObject("mensaje", cli);
-        return modelAndView; 
-    }
-
-
-
 }
