8eafb4ffecd7df46822598ad44d4c80b7c37f246
jonadepr <eljosele4@gmail.com>
2020-02-14 13:43:02 +0100

    manytomany arraylist

--- src/main/java/com/alfonsotienda/holaspring/model/Producto.java
+++ src/main/java/com/alfonsotienda/holaspring/model/Producto.java
@@ -1,12 +1,13 @@
 package com.alfonsotienda.holaspring.model;
 
-import java.util.List;
-
+import java.util.ArrayList;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 
@@ -21,15 +22,21 @@ public class Producto {
     private Integer id;
 
     @Size(max = 300)
-    private String concepto;
+    @NotNull
+    @Column(name = "name", nullable = false)
+    private String nombre;
 
     private Double precio;
 
+    /**Iniciamos la lista para que no sea nula aunque tenga
+     * cero elementos
+     */
     @ManyToMany(mappedBy = "productos")
-    private List<Factura> facturas;
+    private ArrayList<Factura> facturas = new ArrayList<Factura>() {
+    };
 
-    public Producto(@Size(max = 300) String concepto, Double precio) {
-        this.concepto = concepto;
+    public Producto(@Size(max = 300) String nombre, Double precio) {
+        this.nombre = nombre;
         this.precio = precio;
     }
 
@@ -41,12 +48,12 @@ public class Producto {
         this.id = id;
     }
 
-    public String getConcepto() {
-        return concepto;
+    public String getNombre() {
+        return nombre;
     }
 
-    public void setConcepto(String concepto) {
-        this.concepto = concepto;
+    public void setNombre(String concepto) {
+        this.nombre = concepto;
     }
 
     public Double getPrecio() {
